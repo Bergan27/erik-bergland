1d82644270a25d45ab3b1c8969e3c09132641fe1
Bergan27 <54935266+Bergan27@users.noreply.github.com>
2019-10-24 10:46:15 +0200

    Jag har nu gjort en string som skriven in värdet baklänges genom det sista ordet för och näst sista näst först och så vidare

--- Homework/Vecka38/v43/uppgift2.java
+++ Homework/Vecka38/v43/uppgift2.java
@@ -1,21 +1,15 @@
 
 public class uppgift2 {
-		
+	public static void main(String[] args) {
+		String str = "Johan";
+		System.out.println(rerverse(str));
+	}
 	
-	public stativ 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public static String rerverse(String str) {
+	String reverseStr = "";
+	for(int i = str.length() - 1; i >= 0; i --) {
+		 reverseStr += str.charAt(i);	
+	}
+	return(reverseStr);
+	}
 }
