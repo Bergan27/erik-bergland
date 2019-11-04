7a104fa82045daaf682e2499a1418061788770fb
Bergan27 <54935266+Bergan27@users.noreply.github.com>
2019-11-04 11:02:14 +0100

    Update v43.java

--- Homework/Vecka38/v43/v43.java
+++ Homework/Vecka38/v43/v43.java
@@ -1,30 +1,27 @@
-
+import java.util.Scanner;
+import java.lang.Math;
 public class v43 {
 	
 	public static void main(String[] args) {
-		System.out.println(volume(5)+" Liter");
+			System.out.println(volume(5)+" Liter");
+			String str = "Johan";
+			System.out.println(rerverse(str));
+			
+		}
 		
-	}
-	
 	public static double volume(double radius){
-		
-		
-		double volume = (4*Math.PI *Math.pow(radius, 3))/3;
-		return volume;
-		
-		
-		
-		
-	
-	
-	
-	
-	
+		return (4 * Math.PI * Math.pow(radius, 3))/3;
 	
 	}
 	
-	}
-	
+	public static String rerverse(String str) {
+		String reverseStr = "";
+		for(int i = str.length() - 1; i >= 0; i --) {
+		 reverseStr += str.charAt(i);	
+		}
+		return(reverseStr);
+}
+}
 	
 	
 	
