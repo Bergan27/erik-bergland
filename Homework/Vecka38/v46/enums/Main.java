81f9c00b92db2c7e7bd5fcfff70a877808635dfb
Bergan27 <54935266+Bergan27@users.noreply.github.com>
2019-11-18 10:57:32 +0100

    Update Main.java

--- Homework/Vecka38/v46/enums/Main.java
+++ Homework/Vecka38/v46/enums/Main.java
@@ -1,11 +1,18 @@
 package enums;
 
 public class Main {
+	 
+	static double G = (6.67408 * Math.pow(10, -11)) ;    //Gravitationskonstanten.
+	static double R  = (8.3145) ; 						// Almänna gaskonstanten.
+	static double p0 = (1000)  ; 						// lufttrycket vid havsytan.
+	static double c = (2.99792458)  ; 					// Ljusets hastighet i vakum.
+	static double g_swe = (9.82) ; 						//Tyngdaccelerationen för Sverige.
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		
 		System.out.println(fahrenheitToCelsius(50));
 		System.out.println(kelvinToCelsius(0));
+		System.out.println(fluidPreassure(FluidTable.WATER,10));
 	}
 	
 	public static double fahrenheitToCelsius(double fahrenheit) {
@@ -19,6 +26,16 @@ public class Main {
 		return celsius;
 		// Här är min andra metod där jag räknar ut hur många grader celcius det är om det är -273.15 grader kelvin
 	}
+	
+	public static double fluidPreassure(FluidTable fluid, double deep) {
+		double fluidPressure = fluid.density * 	g_swe * deep;
+		return fluidPressure;
+
+	}
+	
+
+	
+
 
 }
 
