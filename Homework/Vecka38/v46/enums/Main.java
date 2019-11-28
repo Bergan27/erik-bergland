9c83054539eeae7555500f02d2076b8cf2e9be24
Bergan27 <54935266+Bergan27@users.noreply.github.com>
2019-11-28 10:32:26 +0100

    Update Main.java
    Java doc

--- Homework/Vecka38/v46/enums/Main.java
+++ Homework/Vecka38/v46/enums/Main.java
@@ -30,105 +30,177 @@ public class Main {
 		System.out.println(velocityToHeight(9.82));
 	
 	}
-	
+	/**
+	 * 
+	 * @param fahrenheit = A form of measuring degrees or heat/cold 
+	 * @return
+	 */
 	public static double fahrenheitToCelsius(double fahrenheit) {
 		
 		double celsius = (fahrenheit - 32) * 5 / 9;
 		return celsius;
 		// Här är den första metoden där jag räknar ut  hur många grader celcius om det är 50 grader fahrenheit
 	}
-
+	/**
+	 * 
+	 * @param kelvin = A form of measuring degrees or heat/cold 
+	 * @return
+	 */
 	public static double kelvinToCelsius(double kelvin) {
 		
 		double celsius = (kelvin - 273.15);
 		return celsius;
 		// Här är min andra metod där jag räknar ut hur många grader celcius det är om det är -273.15 grader kelvin
 	}
-	
+	/**
+	 * 
+	 * @param fluid = The specific fluid and its capabilities 
+	 * @param deep = The depth of the measuring in meters (M)
+	 * @return
+	 */
 	public static double fluidPreassure(FluidTable fluid, double deep) {
 		
 		double fluidPressure = fluid.density * 	g_swe * deep;
 		return fluidPressure;
 		// Här är min tredje metod där jag räknar ut trycket under vatten på 10 meters djup
 	}
-	
+	/**
+	 * 
+	 * @param deep = The depth of the measuring in meters (M)
+	 * @return
+	 */
 	public static double pressureUnderWater(double deep){
 		
 		double pressureUnderWater = FluidTable.WATER.density * g_swe * deep;
 		return pressureUnderWater;
 		// Här är min fjärde metod som räknar ut trycket under vattnet på 10 meters djup
 	}
-	
+	/**
+	 * 
+	 * @param mass = The mass off the object
+	 * @param velocity  = The speed of the object measured in (km/h)
+	 * @return
+	 */
 	public static double kineticEnergy (double mass , double velocity ) {
 		
 		double kineticEnergy = (mass * Math.pow(velocity, 2 )) / 2;
 		return kineticEnergy ;
 		//Här är min femte medod som räknar ut den kinetiska energin för ett föremål med en hastighet
 	}
-	
+	/**
+	 * 
+	 * @param mass = The mass off the object
+	 * @param height = The height of the measuring in meters (M)
+	 * @return
+	 */
 	public static double potentialEnergy (double mass , double height) {
 		
 		double potentialEnergy = mass * g_swe * height;
 		return potentialEnergy;
 		//Här är min sjätte metod där jag räkanar ut den potensiella energin för ett föremål
 	}
-	
+	/**
+	 * 
+	 * @param height = The height of the measuring in meters (M)
+	 * @return
+	 */
 	public static double fallSpeed (double height) {
 		
 		double fallSpeed = Math.sqrt(2 * g_swe * height);
 		return fallSpeed;
 		//Här är min sjunde metod där jag räkar ut hastigheten på ett fallande objekt
 	}
-	
+	/**
+	 * 
+	 * @param first = The first value 
+	 * @param last = The second value 
+	 * @return
+	 */
 	public static double delta (double first, double last) {
 		
 		double delta = (last - first);
 		return delta;
 		//Här är min åttånde metod där jag räknar ut det andra värdet minus det första
 	}
-	
+	/**
+	 * 
+	 * @param gas = The gas and its capabilities
+	 * @param volume = The volume of the object measured in liters (L)
+	 * @return
+	 */
 	public static double volumeToMass (GasTable gas, double volume ) {
 		
 		double airdensity = gas.density * volume;
 		return airdensity ;
 		//Här är min nionde metod där jag räknar ut massan 
 	}
-	
+	/**
+	 * 
+	 * @param solid = The material and its capabilities 
+	 * @param volume = The volume of the object measured in liters (L)
+	 * @return
+	 */
 	public static double volumeToMass(SolidTable solid, double volume) {
 		
 		double massdensity = solid.density * volume;
 		return massdensity;
 		//Här har jag min tionde metod som räknar ut vikten från volymne och densiteten
 	}
-	
+	/**
+	 * 
+	 * @param distance = The distance measured in meters (M)
+	 * @param time = The time spawn measured in seconds (S)
+	 * @return
+	 */
 	public static double svtVelocity(double distance, double time) {
 		
 		double speed = distance / time;
 		return speed;
 		//Här är min elfte metod som räknar ut medelhastigheten
 	}
-	
+	/**
+	 * 
+	 * @param velocity = The speed of the object measured in (km/h)
+	 * @param time = The time spawn measured in seconds (S)
+	 * @return
+	 */
 	public static double svtDistance(double velocity, double time) {
 		
 		double distance = velocity * time;
 		return distance;
 		//Här är min tolfte metod som räknar ut sträckan från hastigheten och tiden
 	}
-	
+	/**
+	 * 
+	 * @param velocity = The speed of the object
+	 * @param time = The time spawn measured in seconds (S)
+	 * @return
+	 */
 	public static double svtTime(double velocity, double time) {
 		
 		double Time = velocity / time;
 		return Time;
 		// Metod nr 13 som räknar ut tiden från hasighet och sträcka
 	}
-	
+	/**
+	 * 
+	 * @param force = The force that is measured in newton (N)
+	 * @param distance = The distance measured in meters (M)
+	 * @return
+	 */
 	public static double work(double force, double distance) {
 		
 		double workAmount = force * distance;
 		return workAmount;
 		//16 de metoden som räknar ut storlekten på jobbet som utförs
 	}
-	
+	/**
+	 * 
+	 * @param solid = The material and its capabilities 
+	 * @param mass = The mass off the object
+	 * @param deltaT = The change in time
+	 * @return
+	 */
 	public static double  heat(SolidTable solid, double mass, double deltaT) {
 		
 		double temp = solid.heatCapacity* mass * deltaT;
@@ -159,9 +231,13 @@ public class Main {
 		
 		double burn = gas.heatCapacity * mass * deltaT;
 		return burn; 
-		//19de metoden som räknar ut hur mycket energi som krävs för att värma en massa till
+		//19de metoden som räknar ut hur mycket energi som krävs för att värma en massa 
 	}
-	
+	/**
+	 * 
+	 * @param velocity = The speed of the object
+	 * @return
+	 */
 	public static double velocityToHeight(double velocity) {
 		
 		double height = (Math.pow(Math.sin(1.570796),2 )* Math.pow(velocity, 2))/ (2*g_swe);
