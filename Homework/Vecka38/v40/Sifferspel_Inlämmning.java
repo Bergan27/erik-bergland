db30bfb4124c86ba21142bda5f4e5c7c082a3c2c
Bergan27 <54935266+Bergan27@users.noreply.github.com>
2020-03-21 17:37:04 +0100

    Create Sifferspel_Inlämmning.java
    Början till talspel

---
+++ Homework/Vecka38/v40/Sifferspel_Inlämmning.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Sifferspel_Inlämmning {
+	
+
+	static Scanner input = new Scanner(System.in);
+	static String spelarNamn = "";
+	static int spelarSvar;
+	static int SpelRegler;
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		talSpelet();
+
+	}
+	public static void talSpelet() {
+		System.out.println("Välkommen till talspelet!");
+		
+
+		System.out.println("Vad vill du heta?");
+		spelarNamn = input.nextLine();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
