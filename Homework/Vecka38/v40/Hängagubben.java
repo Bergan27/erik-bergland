ec7af3eb8bf082adccd726c236e7f2ab47c60d86
Erik Bergland <54935266+Bergan27@users.noreply.github.com>
2020-05-14 19:36:14 +0200

    Create Hängagubben.java
    En metod som hälsar dig välkommen

---
+++ Homework/Vecka38/v40/Hängagubben.java
@@ -0,0 +1,15 @@
+
+public class Hängagubben {
+
+	public static void main(String[] args) {
+		Välkommen();
+
+	}
+
+	public static void Välkommen() {
+		System.out.println("Välkommen till hängagubben!, du kommer att få välja mellan 3 olika svårhetsgrader"
+				+ " Du kommer att få gissa 6 gånger vad ordet är, om du inte lyckas innan det så hängs gubben och du förorar"
+				+ "Om du lyckas innan gubben hängs så vinner du och gubben överlever");
+	}
+
+}
